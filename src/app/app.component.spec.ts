7fd414d15a40e279b6932f161a14ef0d2e2b9b7c
Anthony <s35391@stagiaire.aelion.fr>
2024-01-30 10:18:02 +0100

    Customize layout

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-init');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('p')?.textContent).toContain('angular-init');
-  });
 });
